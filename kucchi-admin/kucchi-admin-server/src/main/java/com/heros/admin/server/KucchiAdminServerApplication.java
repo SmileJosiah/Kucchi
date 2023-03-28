481065a58421f19886c1996ec75734446a17e08d
hz21107381 <zhuxingyi@huize.com>
2023-03-28 18:24:56 +0800

    add(project):初始化项目

---
+++ kucchi-admin/kucchi-admin-server/src/main/java/com/heros/admin/server/KucchiAdminServerApplication.java
@@ -0,0 +1,16 @@
+package com.heros.admin.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuxingyi
+ * @date 28/3/2023 下午2:10
+ */
+@SpringBootApplication
+public class KucchiAdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KucchiAdminServerApplication.class, args);
+    }
+}
---
+++ kucchi-service/kucchi-service-server/src/main/java/com/heros/service/server/KucchiServiceServerApplication.java
@@ -0,0 +1,15 @@
+package com.heros.service.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuxingyi
+ * @date 28/3/2023 下午1:50
+ */
+@SpringBootApplication
+public class KucchiServiceServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KucchiServiceServerApplication.class, args);
+    }
+}
